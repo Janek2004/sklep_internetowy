41c4698fb6d9648c2fbbd677eda87554e5d30f5a
Janek2004 <115580328+Janek2004@users.noreply.github.com>
2022-12-03 19:25:02 +0100

    Summary 2

--- src/Shop/Recommended/RecommendedItem.js
+++ src/Shop/Recommended/RecommendedItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const RecommendedItem = ({ pos, img, title }) => {
+const RecommendedItem = ({ pos, img, title, price }) => {
 
     const wrapperClassName = `Recommended-item shadow rounded-4 p-2 d-flex flex-column pb-5`
 
@@ -12,6 +12,7 @@ const RecommendedItem = ({ pos, img, title }) => {
 
     return (
         <label onClick={onClick} htmlFor={`r-c-item-${pos}`} id={`item-${pos}`} className={wrapperClassName}>
+            <span className="Recommended-item-price h6">{price}</span>
             <img className="Recommended-item-img" src={img} alt="bucik" />
             <h3 className="Recommended-item-title mt-auto">{title}</h3>
             <h4 id={`click-${pos}`} className="Recommended-item-click d-non">Kliknij aby zobaczyć</h4>
--- src/Shop/Recommended/index.js
+++ src/Shop/Recommended/index.js
@@ -13,11 +13,11 @@ const Recommended = () => {
                 <input type="radio" name="slider" id="r-c-item-4" />
                 <input type="radio" name="slider" id="r-c-item-5" />
             <div className="Recommended mt-5">
-                <RecommendedItem title='Jordan Jumpman Pro' img='https://static.nike.com/a/images/t_default/f6ed1490-9e36-4994-b32d-098653245611/buty-meskie-jordan-jumpman-pro-j5vqXB.png' pos={1} />
-                <RecommendedItem title='Jordan Max Aura 4' img='https://static.nike.com/a/images/t_default/030bc0e1-869b-4751-b2fe-a85c78393831/buty-meskie-jordan-max-aura-4-bzn4qR.png' pos={2} />
-                <RecommendedItem title='Jordan 1 Lucky Green' img='https://www.sneakfreak.pl/userdata/public/gfx/25757/4039.png' pos={3} />
-                <RecommendedItem title='Air Jordan 1 Mid' img='https://static.nike.com/a/images/t_default/2016a636-2953-41b4-b496-55263f2b26bc/buty-air-jordan-1-mid-nTNrnK.png' pos={4} />
-                <RecommendedItem title='Jordan 1 Low Court Purple' img='https://onsetshoes.com/wp-content/uploads/2021/02/air-jordan-1-low-court-purple-white-1-1000.png' pos={5} />
+                <RecommendedItem price='329 zł' title='Jordan Jumpman Pro' img='https://static.nike.com/a/images/t_default/f6ed1490-9e36-4994-b32d-098653245611/buty-meskie-jordan-jumpman-pro-j5vqXB.png' pos={1} />
+                <RecommendedItem price='989 zł' title='Jordan Max Aura 4' img='https://static.nike.com/a/images/t_default/030bc0e1-869b-4751-b2fe-a85c78393831/buty-meskie-jordan-max-aura-4-bzn4qR.png' pos={2} />
+                <RecommendedItem price='399 zł' title='Jordan 1 Lucky Green' img='https://www.sneakfreak.pl/userdata/public/gfx/25757/4039.png' pos={3} />
+                <RecommendedItem price='699 zł' title='Air Jordan 1 Mid' img='https://static.nike.com/a/images/t_default/2016a636-2953-41b4-b496-55263f2b26bc/buty-air-jordan-1-mid-nTNrnK.png' pos={4} />
+                <RecommendedItem price='569 zł' title='Jordan 1 Low Court Purple' img='https://onsetshoes.com/wp-content/uploads/2021/02/air-jordan-1-low-court-purple-white-1-1000.png' pos={5} />
             </div>
         </div>
     )
